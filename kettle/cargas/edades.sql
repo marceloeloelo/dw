4c3a40c7a46927a198f98891fda0529a680da584
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-06-23 20:30:40 -0300

    fix para salud.sql, carga infoPersonas - WIP

--- kettle/cargas/edades.sql
+++ kettle/cargas/edades.sql
@@ -5,5 +5,5 @@ INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (4,'22 a 30');
 INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (5,'30 a 40');
 INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (6,'40 a 50');
 INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (7,'50 a 60');
-INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (9,'60 a 70');
-INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (10,'mas de 70');
+INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (8,'60 a 70');
+INSERT INTO ECH_DW.edades(idEdades, edad) VALUES (9,'mas de 70');
