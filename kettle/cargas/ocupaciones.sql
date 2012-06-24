d0de496ae3a475016f5f4de33ed1f32ea8897538
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2012-06-24 10:06:52 -0300

    update

--- kettle/cargas/ocupaciones.sql
+++ kettle/cargas/ocupaciones.sql
@@ -1,9 +1,9 @@
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (1,'desocupado');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (2,'inactivo');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (3,'asalariado privado');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (4,'asalariado publico');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (5,'miembro de cooperativa de produccion');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (6,'patron');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (7,'cuenta propia sin local o inversion');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (8,'cuenta propia con local o inversion');
-INSERT INTO ocupaciones(idOcupacion, ocupacion) VALUES (9,'programa publico (social) de empleo');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (1,'desocupado');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (2,'inactivo');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (3,'asalariado privado');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (4,'asalariado publico');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (5,'miembro de cooperativa de produccion');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (6,'patron');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (7,'cuenta propia sin local o inversion');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (8,'cuenta propia con local o inversion');
+INSERT INTO ocupaciones(idOcupaciones, ocupacion) VALUES (9,'programa publico (social) de empleo');
--- kettle/cargas/sexos.sql
+++ kettle/cargas/sexos.sql
@@ -1,2 +1,2 @@
-INSERT INTO ECH_DW.sexos(idSexo, sexo) VALUES (1,'Masculino');
-INSERT INTO ECH_DW.sexos(idSexo, sexo) VALUES (2,'Femenino');
+INSERT INTO ECH_DW.sexos(idSexos, sexo) VALUES (1,'Masculino');
+INSERT INTO ECH_DW.sexos(idSexos, sexo) VALUES (2,'Femenino');
